c07184cf087d194e6f29f173e5c3e39f3e75a8bc
aalok.sathe <aalok.sathe@gmail.com>
2019-12-06 01:42:46 -0500

    inherit from queue.Queue for Bottleneck

--- bottleneck.py
+++ bottleneck.py
@@ -5,11 +5,17 @@ people: Nick B., Matthew J., Aalok S.
 In this file we define a useful class to model bottlenecks, 'Bottleneck'
 '''
 
+from queue import Queue
 
-class Bottleneck:
 
-    def __init__(self):
+class Bottleneck(Queue):
+    loc = None, None
+
+    def __init__(self, loc):
         '''
         constructor method
+        ---
+        loc (tuple xy): location (coordinates) of this bottleneck
         '''
-        pass
+        super().__init__()
+        self.loc = loc
