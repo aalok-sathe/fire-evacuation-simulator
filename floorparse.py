2b927a3fe336535cc12f1625e8b6335287ade64e
aalok.sathe <aalok.sathe@gmail.com>
2019-12-08 13:29:56 -0500

    module for parsing floors

---
+++ floorparse.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+from collections import defaultdict
+
+class FloorParser:
+
+    def __init__(self):
+        pass
+
+    def parse(self, floor):
+        '''
+        parses a txt floor
+        '''
+        grid = []
+
+        for row in floor.split('\n'):
+            if not row: continue
+            sqs = row.split(';')
+            rowattrs = [set(sq.strip().split(',')) for sq in sqs]
+            print(rowattrs)
+            grid += [rowattrs]
+
+
+        graph = defaultdict(lambda: {'nbrs': set()})
+
+        R, C = len(grid), len(grid[0])
+       
+        for i in range(R):
+            for j in range(C):
+                attrs = grid[i][j]
+                graph[(i,j)].update({att:int(att in attrs) for att in 'WSBFNP'})
+                
+                for off in {-1, 1}:
+                    if 0 <= i+off < R:
+                        graph[(i,j)]['nbrs'].add((i+off, j))
+
+                    if 0 <= j+off < C:
+                        graph[(i,j)]['nbrs'].add((i, j+off))
+        
+        self.graph = dict(graph.items())
+        return self.graph
+
