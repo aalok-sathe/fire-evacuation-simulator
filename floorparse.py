2902faf1ca68ec39ff75cf34a6f6c76afebe7c44
as9kc <aalok.sathe@richmond.edu>
2019-12-13 22:49:09 -0500

    add a method to output a graph to string

--- floorparse.py
+++ floorparse.py
@@ -24,19 +24,39 @@ class FloorParser:
         graph = defaultdict(lambda: {'nbrs': set()})
 
         R, C = len(grid), len(grid[0])
-       
+
         for i in range(R):
             for j in range(C):
                 attrs = grid[i][j]
                 graph[(i,j)].update({att:int(att in attrs) for att in 'WSBFNP'})
-                
+
                 for off in {-1, 1}:
                     if 0 <= i+off < R:
                         graph[(i,j)]['nbrs'].add((i+off, j))
 
                     if 0 <= j+off < C:
                         graph[(i,j)]['nbrs'].add((i, j+off))
-        
+
         self.graph = dict(graph.items())
         return self.graph
 
+
+    def tostr(self, graph):
+        '''
+        '''
+        r, c = 0, 0
+        for loc, attrs in graph.items():
+            r = max(r, loc[0])
+            c = max(c, loc[1])
+        r, c = r+1, c+1
+
+        s = ''
+        for r_ in range(r):
+            for c_ in range(c):
+                sq = graph[(r_, c_)]
+                # this =
+                att = ','.join([a for a in sq if a in 'BNSFWP' and sq[a]])
+                s += '{:>4}'.format(att)
+            s += '\n'
+
+        return s
